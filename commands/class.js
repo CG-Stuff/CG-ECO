050dd0730bdcbc287d15a72cc3a9e717bc5fe476
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2020-04-21 21:54:59 +0700

    Add the embed

--- commands/class.js
+++ commands/class.js
@@ -3,4 +3,7 @@ const Discord = require('discord.js')
 module.export= {
 name: 'class',
 description: 'Pick a class that you want.',
+execute(message, args, client){
+  const embedded = new Discord.RichEmbed()
+  .setTitle('
   
